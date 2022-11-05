1bc29585987a145d145c993ac6e627ec1f1d8480
Daniel Neaves <dan@danielneaves.com>
2022-11-05 17:40:36 +0000

    Test to trigger build

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ function buildProtoForTypes {
       setupBranch $REPOPATH/$reponame
 
       # Use the docker container for the language we care about and compile
-      docker run -v "$(pwd):/defs" namely/protoc-$lang --with-docs markdown,DOCS.md --lint
+      docker run -v "$(pwd):/defs" namely/protoc-$lang
 
       # We will also generate documentation
       docker run -v "$(pwd):/out" -v "$(pwd):/protos" \
