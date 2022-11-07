5d2628b20ecb9258c6c275f037458ad93832ef19
Daniel Neaves <dan@danielneaves.com>
2022-11-07 15:16:05 +0000

    Change the docker image and add some options like linting

--- build.sh
+++ build.sh
@@ -62,7 +62,6 @@ function buildProtoForTypes {
       # Copy the generated files out of the pb-* path into the repository
       # that we care about
       cp -R gen/pb-$lang/* $REPOPATH/$reponame/
-      cp -R gen/doc/DOCS.md $REPOPATH/$reponame/
 
       commitAndPush $REPOPATH/$reponame
     done < .protolangs
